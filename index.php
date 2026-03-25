8e7e563250436e69213a6063157d7d82732aec8f
Luis <lcaballero.1810@unimar.edu.ve>
2026-03-25 17:18:19 -0500

    nebula

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es-VE">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="/assets/gamepad.png" type="image/x-icon">
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <title>NEBULA GAMING</title>
+</head>
+<body>
+
+ <head>
+    <header>
+        <nav class="navegation nav-expand-lg bg-white">
+            <div class="nav-nag ms-4 me-auto">
+                <a href="" class="nav-brang"><h3 class="text-warning">NEBULA <span class="text-black">GAMING</span></h3></a>
+                <form method="GET" action="" class="d-flex" type="search">
+                    <input type="search" class="form-control" placeholder="Escribe un producto" name="results" required>
+                    <button class="btn btn-darck" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+                </form>
+                
+            </div>
+        </nav>
+    </header>
+ </head>
+    
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/a5601269a3.js" crossorigin="anonymous"></script>
+</html>
