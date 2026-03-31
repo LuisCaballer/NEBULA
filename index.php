52618fe368a8c3d78e8d5a02e160266ad4dae843
LuisCaballer <lcaballero.1810@unimar.edu.ve>
2026-03-31 08:02:34 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <link rel="shortcut icon" href="/assets/gamepad.png" type="image/x-icon">
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <title>NEBULA GAMING</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
 
@@ -13,7 +14,7 @@
     <header>
         <nav class="navegation nav-expand-lg bg-white">
             <div class="nav-nag ms-4 me-auto">
-                <a href="" class="nav-brang"><h3 class="text-warning">NEBULA <span class="text-black">GAMING</span></h3></a>
+                <a href="" class="nav-brang"><h3 class="text-blue">NEBULA <span class="text-black">GAMING</span></h3></a>
                 <form method="GET" action="" class="d-flex" type="search">
                     <input type="search" class="form-control" placeholder="Escribe un producto" name="results" required>
                     <button class="btn btn-darck" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
