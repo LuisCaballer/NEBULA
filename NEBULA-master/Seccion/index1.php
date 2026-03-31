594df2f722e885519e90b0f5ab82a7280216913b
LuisCaballer <lcaballero.1810@unimar.edu.ve>
2026-03-31 14:16:12 -0500

    Add files via upload

---
+++ NEBULA-master/Seccion/index1.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iniciar Sección</title>
+    <link rel="stylesheet" href="style1.css">
+</head>
+<body>
+    <div class="formulario">
+        <h1>Inicio de Sección</h1>
+        <form method="post">
+            <div class="username">
+            <input type="text" required>
+            <label>Nombre de Usuario</label>
+        </div>
+           <div class="username">
+           <input type="text" required>
+        <label>Contraseña</label>
+         </div>
+    <div class="recordar">Olvido su contraseña?</div>
+    <input type="submit" value="Iniciar">
+    <div class="registrarse">
+        quiero hacer el <a href="#">Resitro</a>
+    </div>
+    </form>
+        
+
+    
+</body>
+</html>
